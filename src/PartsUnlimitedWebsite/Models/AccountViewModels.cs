8db780fa7cd995ea3f40ac8edd5a43ac8305ae90
ujjwalbarman <ujjwal.barman@gmail.com>
2022-10-18 13:50:11 +0100

    Added a line in AccountViewModels.cs

--- src/PartsUnlimitedWebsite/Models/AccountViewModels.cs
+++ src/PartsUnlimitedWebsite/Models/AccountViewModels.cs
@@ -1,6 +1,6 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
+// Adding a line for testing
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
